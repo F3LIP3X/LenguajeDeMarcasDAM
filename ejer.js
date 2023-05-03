2becb69299e9c401712e2dc3558b5218310d8ac1
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2023-05-03 21:25:26 +0200

    1.0
    :+1:

---
+++ ejer.js
@@ -0,0 +1,10 @@
+for (let i = 1; i < 50; i = i + 2) {
+    document.write(
+            "<tr>",
+                "<td>",i,"</td>",
+                "<td>",i,"</td>",
+            "</tr>",
+    )
+    
+}
+
