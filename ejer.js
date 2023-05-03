cb6769bf06d6f0eb0d93066d3f1e979883f9d8b3
Felipe <76501827+F3LIP3X@users.noreply.github.com>
2023-05-03 21:28:35 +0200

    1.1
    :+1:

--- ejer.js
+++ ejer.js
@@ -2,7 +2,7 @@ for (let i = 1; i < 50; i = i + 2) {
     document.write(
             "<tr>",
                 "<td>",i,"</td>",
-                "<td>",i,"</td>",
+                "<td>",i+1,"</td>",
             "</tr>",
     )
     
